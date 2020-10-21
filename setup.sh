0cfabc41ed8a6773654d0a4f774468099587b716
ttamesha <ttamesha@oa-c5.msk.21-school.ru>
2020-10-21 18:59:51 +0300

    maybe final

--- setup.sh
+++ setup.sh
@@ -1,23 +1,6 @@
-SERVICES="mysql phpmyadmin nginx wordpress ftps influxdb grafana telegraf"
+#minikube delete;
 
-# Colors
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-BLUE='\033[34m'
-PURPLE='\033[35m'
-CYAN='\033[36m'
-DEFAULT='\033[0m'
-
-function create_svc()
-{
-	docker build -t $1-img ./$1/
-	kubectl create -f $1/$1.yaml
-}
-
-minikube delete;
-
-minikube start --driver=virtualbox --disk-size=4000MB;
+minikube start --driver=virtualbox --disk-size=5000MB;
 minikube addons enable metrics-server;
 minikube addons enable metallb;
 minikube addons enable dashboard;
@@ -32,23 +15,16 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 kubectl apply -f ./srcs/metallb.yaml ;
 
 #Build images and apply deployments
-#cd srcs
-#for service in $services
-#do
-#	echo $service 'starting'
-#	create_svc $service
-#done
-
-#docker build -t wordpress-img ./srcs/wordpress/ ;
-#kubectl apply -f ./srcs/wordpress/wordpress.yaml ;
-#docker build -t mysql-img ./srcs/mysql/ ;
-#kubectl apply -f ./srcs/mysql/mysql.yaml ;
-#docker build -t phpmyadmin-img ./srcs/phpmyadmin/ ;
-#kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml ;
-#docker build -t nginx-img ./srcs/nginx/ ;
-#kubectl apply -f ./srcs/nginx/nginx.yaml ;
-#docker build -t ftps-img ./srcs/ftps/ ;
-#kubectl apply -f ./srcs/ftps/ftps.yaml ;
+docker build -t wordpress-img ./srcs/wordpress/ ;
+kubectl apply -f ./srcs/wordpress/wordpress.yaml ;
+docker build -t mysql-img ./srcs/mysql/ ;
+kubectl apply -f ./srcs/mysql/mysql.yaml ;
+docker build -t phpmyadmin-img ./srcs/phpmyadmin/ ;
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml ;
+docker build -t nginx-img ./srcs/nginx/ ;
+kubectl apply -f ./srcs/nginx/nginx.yaml ;
+docker build -t ftps-img ./srcs/ftps/ ;
+kubectl apply -f ./srcs/ftps/ftps.yaml ;
 docker build -t influxdb-img ./srcs/influxdb/ ;
 kubectl apply -f ./srcs/influxdb/influxdb.yaml ;
 docker build -t grafana-img ./srcs/grafana/ ;
